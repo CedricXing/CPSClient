ee61998843e405419149e44628172ffe22a1d30b
Qcer17 <qin_tianyu@hotmail.com>
2018-04-08 17:07:08 +0800

    replace trie.h

--- velocity/integrate/trie.h
+++ velocity/integrate/trie.h
@@ -11,13 +11,8 @@ struct Trie {
 typedef struct Trie Trie;
 
 void int2char(unsigned int x, unsigned char* buf) {
-<<<<<<< HEAD
-	int i;
-	for (i = 0; i < 4 ; ++i) {
-=======
     int i = 0;
 	for (; i < 4 ; ++i) {
->>>>>>> 9267393a24f29a306a9bc5d8ab978b97cca2b731
 		buf[i] = x % 256;
 		x /= 256;
 	}
@@ -25,13 +20,8 @@ void int2char(unsigned int x, unsigned char* buf) {
 
 void insert(Trie* trie, unsigned char* buf, int id) {
 	int p = 0;
-<<<<<<< HEAD
-	int i;
-	for (i = 0; i < 4; ++i) {
-=======
 	int i = 0;
 	for (; i < 4; ++i) {
->>>>>>> 9267393a24f29a306a9bc5d8ab978b97cca2b731
 		if (trie->son[p][buf[i]] == -1) {
 			trie->id[++trie->tot] = 0;
 			memset(trie->son[trie->tot], -1, SIZE*sizeof(int));
@@ -47,13 +37,8 @@ void init(Trie* trie, const char* filename) {
 	memset(trie->son[0], -1, SIZE*sizeof(int));
 
 	FILE* file = fopen(filename, "r");
-<<<<<<< HEAD
-	int i;
-	for (i = 0; i < 126; ++i) {
-=======
 	int i = 0;
 	for (; i < 126; ++i) {
->>>>>>> 9267393a24f29a306a9bc5d8ab978b97cca2b731
 		unsigned int cid;
 		fscanf(file, "%u", &cid);
 		unsigned char* buf = (char*)malloc(4*sizeof(char));	
@@ -66,13 +51,8 @@ int query(Trie* trie, int cid) {
 	int p = 0;
 	unsigned char* buf = (char*)malloc(4*sizeof(char));
 	int2char(cid, buf);
-<<<<<<< HEAD
-	int i;
-	for (i = 0; i < 4; ++i) {
-=======
 	int i = 0;
 	for (; i < 4; ++i) {
->>>>>>> 9267393a24f29a306a9bc5d8ab978b97cca2b731
 		p = trie->son[p][buf[i]];
 	}
 	return trie->id[p];
@@ -80,16 +60,10 @@ int query(Trie* trie, int cid) {
 
 void test(Trie* trie, const char* filename) {
 	FILE* file = fopen(filename, "r");
-<<<<<<< HEAD
-	int i;
-	for (i = 0; i < 126; ++i) {
-=======
 	int i = 0;
 	for (; i < 126; ++i) {
->>>>>>> 9267393a24f29a306a9bc5d8ab978b97cca2b731
 		unsigned int cid;
 		fscanf(file, "%u", &cid);
 		printf("%u\t->\t%d\n", cid, query(trie, cid));
 	}
 }
-
